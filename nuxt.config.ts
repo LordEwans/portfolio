32d5f20d18175bc1af64cc2fa0217de3d802c73f
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-07-27 20:59:55 +0100

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false, //When false  target: "static",
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/main.css"],
 });
