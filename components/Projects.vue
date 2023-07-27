90964b3e30a5ee001cc29e5474261179e576da16
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-07-27 09:38:21 +0100

    Update

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,20 +1,20 @@
 <template class="h-screen overflow-hidden flex items-center justify-center">
   <div class="flex flex-col m-auto p-auto">
     <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
-      <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
+      <div class="flex flex-nowrap justify-evenly">
         <div class="inline-block px-3">
           <div
-            class="w-64 h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            class="w-52 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
           ></div>
         </div>
         <div class="inline-block px-3">
           <div
-            class="w-64 h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            class="w-56 h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
           ></div>
         </div>
         <div class="inline-block px-3">
           <div
-            class="w-64 h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            class="w-64 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-slate-900 hover:shadow-xl transition-shadow duration-300 ease-in-out"
           ></div>
         </div>
       </div>
