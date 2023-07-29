94fd16a7800f140f6322ecd2ba7f0d79bb7fc6de
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-07-29 20:36:30 +0100

    Update

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <Navigation class="self-start" />
   <div
-    class="w-full flex justify-center items-center m-auto h-[14em] min-h-0 pb-[25%] pt-[12.5%] sm:pt[25%]"
+    class="w-full flex justify-center items-center m-auto h-[20em] md:h-[14em] min-h-0 pb-[25%] pt-[12.5%] sm:pt[25%]"
   ></div>
 </template>
 
